55242ca99e285e90f83ec35bf0e40e9cb5e80187
Cameron Williams <cameronrw.nz@gmail.com>
2018-12-13 23:10:37 +0700

    SearchVariety
    Providing ability to search Google, youtube or reddit.

--- src/Widgets/SearchBar.js
+++ src/Widgets/SearchBar.js
@@ -1,11 +1,57 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props) 
+
+        this.state = {
+            currentProviderIndex: 0
+        }
+
+        this.handleProviderChange = this.handleProviderChange.bind(this)
+    }
+
+    handleProviderChange(event, maxNoOfProviders) {
+        var currentProviderIndex = this.state.currentProviderIndex;
+        if (event.keyCode === 38){
+            currentProviderIndex--;
+        }
+        else if (event.keyCode === 40) {
+            currentProviderIndex++;
+        }
+        
+        this.setState({
+            currentProviderIndex: (currentProviderIndex % maxNoOfProviders + maxNoOfProviders) % maxNoOfProviders 
+        })
+    }
+
     render() {
+        var providers = [
+            {
+                action: "https://www.google.com/search",
+                placeholder: "Search Google"
+            },
+            {
+                action: "https://www.youtube.com/results?search_query",
+                placeholder: "Search Youtube"
+            },
+            {
+                action: "https://www.reddit.com/search",
+                placeholder: "Search Reddit"
+            }
+        ];
+
+        
         return (
             <div>
-                <form action="https://www.google.com/search" method="get" name="searchform">
-                    <input className="search-bar-search" autocomplete="off" type="text" name="q" placeholder="Search"/>
+                <form action={providers[this.state.currentProviderIndex].action} method="get" name="searchform">
+                    <input 
+                        className="search-bar-search" 
+                        autoComplete="off" 
+                        type="text" 
+                        name="q" 
+                        placeholder={providers[this.state.currentProviderIndex].placeholder}
+                        onKeyDown={(event) => this.handleProviderChange(event, providers.length)} />
                 </form>
             </div>
         )
