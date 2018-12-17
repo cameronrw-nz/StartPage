da23bb0b54b43a920bb6c8aa63dc605ec4572a52
Cameron Williams <cameronrw.nz@gmail.com>
2018-12-17 23:23:11 +0700

    gh-pages branch attempt #1
    gh-pages branch attempt #1

---
+++ precache-manifest.5a72a1a86453e46efef611fc8031e81c.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "f857faf05151956a86e6791e667f0f55",
+    "url": "/StartPage/static/media/brokenClouds.f857faf0.svg"
+  },
+  {
+    "revision": "40a814b7da5e9a017aed",
+    "url": "/StartPage/static/css/main.3d9665a9.chunk.css"
+  },
+  {
+    "revision": "f20041d05b4f507aaf6b",
+    "url": "/StartPage/static/js/1.f20041d0.chunk.js"
+  },
+  {
+    "revision": "4348a41df45156bb5a72",
+    "url": "/StartPage/static/js/runtime~main.4348a41d.js"
+  },
+  {
+    "revision": "4082623ae3f18cbd25c66c4045f02edf",
+    "url": "/StartPage/static/media/email.4082623a.svg"
+  },
+  {
+    "revision": "1bb027109345a90a9eab1e929d8669c2",
+    "url": "/StartPage/static/media/github.1bb02710.svg"
+  },
+  {
+    "revision": "5b316389a9bde74262f76de12e3203c0",
+    "url": "/StartPage/static/media/map.5b316389.svg"
+  },
+  {
+    "revision": "45bc938650217ad4f6d0ec0884f0aa6c",
+    "url": "/StartPage/static/media/reddit.45bc9386.svg"
+  },
+  {
+    "revision": "3974a3edd044644ea69807f73d69f58b",
+    "url": "/StartPage/static/media/twitch.3974a3ed.svg"
+  },
+  {
+    "revision": "198b04526ddedfa7493e72f63b3b0887",
+    "url": "/StartPage/static/media/youtube.198b0452.svg"
+  },
+  {
+    "revision": "40a814b7da5e9a017aed",
+    "url": "/StartPage/static/js/main.40a814b7.chunk.js"
+  },
+  {
+    "revision": "922f1f6ed126efda49cf89365f4fb75c",
+    "url": "/StartPage/static/media/clearSky.922f1f6e.svg"
+  },
+  {
+    "revision": "b0cf30aa5b653bdad27c2c6b920ab5bc",
+    "url": "/StartPage/static/media/fewClouds.b0cf30aa.svg"
+  },
+  {
+    "revision": "ca88433df1b6ffb90946e7d361405acc",
+    "url": "/StartPage/static/media/fog.ca88433d.svg"
+  },
+  {
+    "revision": "531d27ab1f10fa535c9d729ff184a179",
+    "url": "/StartPage/static/media/heavyRain.531d27ab.svg"
+  },
+  {
+    "revision": "9a09d698b7e3dd524d84f01303c248e6",
+    "url": "/StartPage/static/media/rain.9a09d698.svg"
+  },
+  {
+    "revision": "844c1a5537f95bb244fa1260a1d59029",
+    "url": "/StartPage/static/media/scatteredClouds.844c1a55.svg"
+  },
+  {
+    "revision": "b2c0182b28245cec439860b6296ce865",
+    "url": "/StartPage/static/media/snow.b2c0182b.svg"
+  },
+  {
+    "revision": "0fff53ee9787a29819f7738389abcf97",
+    "url": "/StartPage/static/media/thunderStorm.0fff53ee.svg"
+  },
+  {
+    "revision": "f8d3166a7b4da50556d2b6b6c37a486e",
+    "url": "/StartPage/index.html"
+  }
+];
