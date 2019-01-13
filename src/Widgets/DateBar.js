8309878a6d57e87aa8fea64835ea1579f02df23d
Cameron Williams <cameronrw.nz@gmail.com>
2019-01-13 20:56:07 +0700

    Adding NZ timezone
    Adding an additional timezone.

--- src/Widgets/DateBar.js
+++ src/Widgets/DateBar.js
@@ -1,18 +1,17 @@
 import React from 'react'
+import moment from 'moment-timezone'
 
 class DateBar extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            date: new Date().toLocaleString('en-GB', {weekday: 'short', year: 'numeric', month: 'long', day: 'numeric'}),
-            time: new Date().toLocaleTimeString('it-IT')
+            dateTime: new Date(),
         };
     }
     componentDidMount() {
         this.interval = setInterval(() => this.setState({ 
-            date: new Date().toLocaleString('en-GB', {weekday: 'short', year: 'numeric', month: 'long', day: 'numeric'}),
-            time: new Date().toLocaleTimeString('it-IT')
+            dateTime: new Date(),
          }), 1000);
       }
 
@@ -21,17 +20,36 @@ class DateBar extends React.Component {
       }
 
     render() {
+        var localDate = this.state.dateTime.toLocaleString('en-GB', {weekday: 'short', year: 'numeric', month: 'long', day: 'numeric'});
+        var localTime = this.state.dateTime.toLocaleTimeString('it-IT');
+        var nzTime = moment(this.state.dateTime.toString()).tz('NZ').format('hh:mm:ss');
+        var nzDate = moment(this.state.dateTime.toString()).tz('NZ').format('ddd, D MMMM YYYY');
+
         return (
-            <div className="date">
-                <div className="date-time">
-                    {this.state.time}
-                </div>
-                <div className="date-formatted">
-                    {this.state.date}
-                </div>
+            <div className="date-block">
+                <DateBlock timeZone="Bangkok, Thailand" time={localTime} date={localDate} />
+                <DateBlock timeZone="Christchurch, New Zealand" time={nzTime} date={nzDate} />
             </div>
           );
     }
+}
+
+function DateBlock({ timeZone, time, date }) {
+    return (
+        <React.Fragment>
+            <div className="date">
+                <div className="date-time">
+                    {time}
+                </div>
+                <div className="date-formatted">
+                    {date}
+                </div>
+                <div className="date-formatted">
+                    {timeZone}
+                </div>
+            </div>
+        </React.Fragment>
+    )
 }
 
 export default DateBar
