470f152448508dcfed185f07ce89511abfb14ccf
Cameron Williams <cameronrw.nz@gmail.com>
2019-09-18 07:29:22 +0700

    Updating Styles
    Updating Styles

---
+++ precache-manifest.7a6a1918cb89197b9bccc2464c2d00b6.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7c57edd720126e4ac6b8c60893f57cf",
+    "url": "/StartPage/static/media/YoutubeIcon.a7c57edd.svg"
+  },
+  {
+    "revision": "8b6f5c04901be1e2c06a",
+    "url": "/StartPage/static/css/main.ebf8b84c.chunk.css"
+  },
+  {
+    "revision": "86a9e9dc9400cde71c23",
+    "url": "/StartPage/static/js/1.86a9e9dc.chunk.js"
+  },
+  {
+    "revision": "4348a41df45156bb5a72",
+    "url": "/StartPage/static/js/runtime~main.4348a41d.js"
+  },
+  {
+    "revision": "d81f3528ecaea1c409e3399ae109c198",
+    "url": "/StartPage/static/media/edit.d81f3528.svg"
+  },
+  {
+    "revision": "4082623ae3f18cbd25c66c4045f02edf",
+    "url": "/StartPage/static/media/email.4082623a.svg"
+  },
+  {
+    "revision": "1bb027109345a90a9eab1e929d8669c2",
+    "url": "/StartPage/static/media/github.1bb02710.svg"
+  },
+  {
+    "revision": "5b316389a9bde74262f76de12e3203c0",
+    "url": "/StartPage/static/media/map.5b316389.svg"
+  },
+  {
+    "revision": "45bc938650217ad4f6d0ec0884f0aa6c",
+    "url": "/StartPage/static/media/reddit.45bc9386.svg"
+  },
+  {
+    "revision": "3974a3edd044644ea69807f73d69f58b",
+    "url": "/StartPage/static/media/twitch.3974a3ed.svg"
+  },
+  {
+    "revision": "198b04526ddedfa7493e72f63b3b0887",
+    "url": "/StartPage/static/media/youtube.198b0452.svg"
+  },
+  {
+    "revision": "b5908130704a1d646504d26a72331c50",
+    "url": "/StartPage/static/media/GoogleIcon.b5908130.svg"
+  },
+  {
+    "revision": "a0b0f5d7b077de73bf1f7463c51d9324",
+    "url": "/StartPage/static/media/RedditIcon.a0b0f5d7.svg"
+  },
+  {
+    "revision": "8b6f5c04901be1e2c06a",
+    "url": "/StartPage/static/js/main.8b6f5c04.chunk.js"
+  },
+  {
+    "revision": "52013f072759b64e4c8b8850bb4afbb8",
+    "url": "/StartPage/static/media/light-on.52013f07.svg"
+  },
+  {
+    "revision": "957f717d8d15c0259fe3a36e9149a1c4",
+    "url": "/StartPage/static/media/light-off.957f717d.svg"
+  },
+  {
+    "revision": "f857faf05151956a86e6791e667f0f55",
+    "url": "/StartPage/static/media/brokenClouds.f857faf0.svg"
+  },
+  {
+    "revision": "922f1f6ed126efda49cf89365f4fb75c",
+    "url": "/StartPage/static/media/clearSky.922f1f6e.svg"
+  },
+  {
+    "revision": "b0cf30aa5b653bdad27c2c6b920ab5bc",
+    "url": "/StartPage/static/media/fewClouds.b0cf30aa.svg"
+  },
+  {
+    "revision": "ca88433df1b6ffb90946e7d361405acc",
+    "url": "/StartPage/static/media/fog.ca88433d.svg"
+  },
+  {
+    "revision": "531d27ab1f10fa535c9d729ff184a179",
+    "url": "/StartPage/static/media/heavyRain.531d27ab.svg"
+  },
+  {
+    "revision": "9a09d698b7e3dd524d84f01303c248e6",
+    "url": "/StartPage/static/media/rain.9a09d698.svg"
+  },
+  {
+    "revision": "844c1a5537f95bb244fa1260a1d59029",
+    "url": "/StartPage/static/media/scatteredClouds.844c1a55.svg"
+  },
+  {
+    "revision": "b2c0182b28245cec439860b6296ce865",
+    "url": "/StartPage/static/media/snow.b2c0182b.svg"
+  },
+  {
+    "revision": "0fff53ee9787a29819f7738389abcf97",
+    "url": "/StartPage/static/media/thunderStorm.0fff53ee.svg"
+  },
+  {
+    "revision": "ebe80716d10ecac11975d3c8381e43bc",
+    "url": "/StartPage/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.11f5526acdbce716833167a8d41c7604.js"
+  "/StartPage/precache-manifest.7a6a1918cb89197b9bccc2464c2d00b6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/StartPage/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
